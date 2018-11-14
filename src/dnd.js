e2e71b3d02edeb2a2c7de89440ee5d55252be410
IlyaTag <bkmz118@mail.ru>
2018-11-14 10:08:13 +0300

    v5 Final

--- src/dnd.js
+++ src/dnd.js
@@ -10,7 +10,6 @@
 /*
  homeworkContainer - это контейнер для всех ваших домашних заданий
  Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
-
  Пример:
    const newDiv = document.createElement('div');
    homeworkContainer.appendChild(newDiv);
@@ -21,7 +20,6 @@ const homeworkContainer = document.querySelector('#homework-container');
  Функция должна создавать и возвращать новый div с классом draggable-div и случайными размерами/цветом/позицией
  Функция должна только создавать элемент и задвать ему случайные размер/позицию/цвет
  Функция НЕ должна добавлять элемент на страницу. На страницу элемент добавляется отдельно
-
  Пример:
    const newDiv = createDiv();
    homeworkContainer.appendChild(newDiv);
@@ -37,8 +35,8 @@ function createDiv() {
     newDiv.style.backgroundColor = `RGB( ${rgb1()}, ${rgb2()}, ${rgb3()} )`;
     newDiv.style.top = Math.round(Math.random() * 1000) + 'px';
     newDiv.style.left = Math.round(Math.random() * 1000) + 'px';
-    newDiv.style.width = Math.round(Math.random(100 - 50) + 50) + 'px';
-    newDiv.style.height = Math.round(Math.random(100 - 50) + 50) + 'px';
+    newDiv.style.width = Math.round(Math.random()*(100 - 50) + 50) + 'px'; 
+    newDiv.style.height = Math.round(Math.random()*(100 - 50) + 50) + 'px';
     newDiv.draggable = 'true';
     newDiv.style.position = 'absolute';
 
@@ -47,7 +45,6 @@ function createDiv() {
 
 /*
  Функция должна добавлять обработчики событий для перетаскивания элемента при помощи drag and drop
-
  Пример:
    const newDiv = createDiv();
    homeworkContainer.appendChild(newDiv);
